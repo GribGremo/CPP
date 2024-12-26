b4a401b55f2756bac6ffbd824edc3656643c1c62
sylabbe <labbesylvainpro@gmail.com>
2024-12-26 12:21:35 +0100

    C

--- CPP_Module_07/ex01/iter.hpp
+++ CPP_Module_07/ex01/iter.hpp
@@ -6,14 +6,19 @@
 /*   By: sylabbe <sylabbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 23:02:46 by sylabbe           #+#    #+#             */
-/*   Updated: 2024/12/25 23:42:12 by sylabbe          ###   ########.fr       */
+/*   Updated: 2024/12/26 10:58:36 by sylabbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 template<typename A, typename L, typename F>
-void    iter(A& address, const L& length, F fn){
+void    iter(A* address, const L& length, F fn){
+    if (address == NULL)
+    {
+        std::cout << "address is NULL" << std::endl;
+        return ;
+    }
     for (L i = 0; i < length; i++){
         fn(address[i]);
     }
--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,13 +6,15 @@
 /*   By: sylabbe <sylabbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 23:02:13 by sylabbe           #+#    #+#             */
-/*   Updated: 2024/12/25 23:47:29 by sylabbe          ###   ########.fr       */
+/*   Updated: 2024/12/26 11:01:25 by sylabbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-
+void incrChar(char& c){
+    ++c;
+}
 
 int main(){
     std::string str = "123456789";
@@ -24,10 +26,34 @@ int main(){
         std::cout << "Value " << i << " of array: " << t[i] << std::endl;
     }
     std::cout << std::endl;
-    // iter(t,10,square<float>);//Cannot do this my array is int type, can/t put float in it
+    // iter(t,10,square<float>);//Cannot do this my array is int type, can't put float in it
     iter(t,10,square<int>);
     
     for(int i = 0; i<10;i++){
         std::cout << "Value " << i << " of array: " << t[i] << std::endl;
     }
+    std::cout << std::endl;
+
+    iter(&str[0],str.size(),incr<char>);// I change address from reference to pointer so he can accept rvalue
+    std::cout <<"str: "<< str << std::endl;
+    std::cout << std::endl;
+
+    iter(&str[0],str.size(),incrChar);// I change address from reference to pointer so he can accept rvalue
+    std::cout << "str: " << str << std::endl;
+    std::cout << std::endl;
+
+    iter(static_cast<char*>(NULL),str.size(),incrChar);//Here compilator doesn't understand NULL as a pointer due to implicit conversion between void* and typed pointer not allowed in C++
 }
+
+/*
+lambda: anonymous function defined inline
+ auto incr = [factor](int& x) { x += factor; };
+
+functor : class or object with an operator() able to be used as a function
+class Increment {
+public:
+    void operator()(int& x) const {
+        x++; 
+    }
+};
+*/
