cd5014b27b0f08889c89371b6373abac46984eb1
sylabbe <labbesylvainpro@gmail.com>
2025-01-14 19:50:30 +0100

    retour?

--- CPP_Module_07/ex02/Array.hpp
+++ CPP_Module_07/ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: grib <grib@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sylabbe <sylabbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 11:43:37 by sylabbe           #+#    #+#             */
-/*   Updated: 2024/12/30 18:19:45 by grib             ###   ########.fr       */
+/*   Updated: 2025/01/14 19:36:08 by sylabbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,51 @@ class Array{
     public:
     Array(){
         array = NULL;
+        n = 0;
     }
+
     Array(const unsigned int s){
+        this->n = s;
         array = new T[s];
+        for (unsigned int i = 0; i < n; i++)
+        {
+            array[i] = T(); //by using T() i will initialise to any default type of my type, if number, put to 0, if a class, will use the default constructor of it
+        }
     }
+
     Array(const Array& src){
-        
+        array = new T[src.n];
+        n = src.n;
+        for (unsigned int i = 0; i < src.n; i++)
+        {
+            array[i] = src.array[i];
+        }
     }
+
     ~Array(){
-        
+        delete[] array;
     }
 
     Array& operator=(const Array& src){
-        
+        if (this == &src)
+            return (*this);
+        delete[] array;
+        n = src.n;
+        array = new T[src.n];// A VOIR
+        for (unsigned int i = 0; i < src.n; i++)
+        {
+            array[i] = src.array[i];
+        }
+        return (*this);
     }
-    const T& operator[](const unsigned int i){
+
+    T& operator[]( unsigned int i){
+        if (i >= n)
+            throw(std::exception());
         return array[i];
     }
     
     private:
+    unsigned int n;
     T *array;
 };
--- CPP_Module_07/ex02/main.cpp
+++ CPP_Module_07/ex02/main.cpp
@@ -3,17 +3,164 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: grib <grib@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sylabbe <sylabbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 12:59:31 by grib              #+#    #+#             */
-/*   Updated: 2024/12/30 13:57:46 by grib             ###   ########.fr       */
+/*   Updated: 2025/01/14 19:49:45 by sylabbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
+void print_cell(Array<int>& a, int i);
+void print_cell(Array<std::string>& a, int i);
+
 int main() {
-    int * a = new int();
-    *a = 1;
-    std::cout << *a <<std::endl;
+    // int * a = new int();
+    // *a = 1;
+    // std::cout << *a <<std::endl;
+    Array<int> intm(4);
+    Array<int> inta(5);
+    Array<int> intp(6);
+
+    std::cout << "~~~~~~~~~~~PRINT DEFAULT ARRAY~~~~~~~~~~~" <<std::endl;
+    print_cell(inta,0);
+    print_cell(inta,1);
+    print_cell(inta,2);
+    print_cell(inta,3);
+    print_cell(inta,4);
+    print_cell(inta,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~PRINT MODIFY ARRAY~~~~~~~~~~~" <<std::endl;
+    inta[0] = 5; 
+    inta[1] = 4; 
+    inta[2] = 3; 
+    inta[3] = 2; 
+    inta[4] = 1; 
+
+    print_cell(inta,0);
+    print_cell(inta,1);
+    print_cell(inta,2);
+    print_cell(inta,3);
+    print_cell(inta,4);
+    print_cell(inta,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~PRINT 4CELLS ARRAY~~~~~~~~~~~" <<std::endl;
+    print_cell(intm,0);
+    print_cell(intm,1);
+    print_cell(intm,2);
+    print_cell(intm,3);
+    print_cell(intm,4);
+    print_cell(intm,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~PRINT 6CELLS ARRAY~~~~~~~~~~~" <<std::endl;
+
+    print_cell(intp,0);
+    print_cell(intp,1);
+    print_cell(intp,2);
+    print_cell(intp,3);
+    print_cell(intp,4);
+    print_cell(intp,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~ASSIGNATION TO BASE ARRAY FOR 4CELLS AND 6CELLS~~~~~~~~~~~" <<std::endl;
+    std::cout << std::endl;
+
+    
+    intm = inta;
+    intp = inta;
+    Array<int> intcpy(inta);
+
+    std::cout << "~~~~~~~~~~~PRINT 4CELLS ARRAY~~~~~~~~~~~" <<std::endl;
+    print_cell(intm,0);
+    print_cell(intm,1);
+    print_cell(intm,2);
+    print_cell(intm,3);
+    print_cell(intm,4);
+    print_cell(intm,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~PRINT 6CELLS ARRAY~~~~~~~~~~~" <<std::endl;
+    print_cell(intp,0);
+    print_cell(intp,1);
+    print_cell(intp,2);
+    print_cell(intp,3);
+    print_cell(intp,4);
+    print_cell(intp,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~PRINT COPY ARRAY~~~~~~~~~~~" <<std::endl;
+    print_cell(intcpy,0);
+    print_cell(intcpy,1);
+    print_cell(intcpy,2);
+    print_cell(intcpy,3);
+    print_cell(intcpy,4);
+    print_cell(intcpy,5);
+    std::cout << std::endl;
+
+    std::cout << "~~~~~~~~~~~PRINT BASE ARRAY~~~~~~~~~~~" <<std::endl;
+    print_cell(inta,0);
+    print_cell(inta,1);
+    print_cell(inta,2);
+    print_cell(inta,3);
+    print_cell(inta,4);
+    print_cell(inta,5);
+    std::cout << std::endl;
+
+    std::string str1("one");
+    std::string str2("two");
+    std::string str3("three");
+    std::string str4("four");
+    std::string str5("five");
+
+    std::cout << "~~~~~~~~~~~CREATION STRING ARRAY~~~~~~~~~~~" <<std::endl;
+
+
+    Array<std::string> stra(5);
+
+    stra[0] = str1;
+    stra[1] = str2;
+    stra[2] = str3;
+    stra[3] = str4;
+    stra[4] = str5;
+
+
+    std::cout << "~~~~~~~~~~~PRINT STRING ARRAY~~~~~~~~~~~" <<std::endl;
+
+    print_cell(stra,0);
+    print_cell(stra,1);
+    print_cell(stra,2);
+    print_cell(stra,3);
+    print_cell(stra,4);
+    print_cell(stra,5);
+}
+
+void print_cell(Array<int>& a, int i){
+    try{
+        std::cout << a[i] << std::endl;
+    }
+    catch(const std::exception&){
+        std::cout << "Invalid index" << std::endl;
+    }
+}
+
+void mod_cell(Array<int>& a, int i, int x){
+    try{
+        a[i] = x;
+    }
+    catch(const std::exception&){
+        std::cout << "Invalid index" << std::endl;
+    }
+}
+
+void print_cell(Array<std::string>& a, int i){
+    try{
+        std::cout << a[i] << std::endl;
+    }
+    catch(const std::exception&){
+        std::cout << "Invalid index" << std::endl;
+    }
 }
