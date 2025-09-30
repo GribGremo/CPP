bba094fc89ba337e0a0e3906bf634d4d5680ccae
sylabbe <sylabbe@student.42angouleme.fr>
2025-09-30 15:46:02 +0200

    malade?

--- CPP_Module_09/ex02/main.cpp
+++ CPP_Module_09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sylabbe <sylabbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 17:20:55 by sylabbe           #+#    #+#             */
-/*   Updated: 2025/09/29 15:41:50 by sylabbe          ###   ########.fr       */
+/*   Updated: 2025/09/30 15:43:28 by sylabbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@ struct result{
 
 template <typename T>
 struct sqc{
-    std::vector<typename T::iterator> seq;
+    std::list<typename T::iterator> seq;
+    int id_min;
+    int id_max;
     int id_seq;
     int id_rec;
     bool full;
@@ -172,13 +174,15 @@ void printStruct(sqc<T> u){
     std::cout << "Sequence: ";
     printCont(u.seq);
     std::cout << "Full: " << u.full << std::endl;
+    std::cout << "Id min: " << u.id_min<< std::endl;
+    std::cout << "Id max: " << u.id_max << std::endl;
     std::cout << "Index vector: " << u.id_seq << std::endl;
     std::cout << "Recursive: " << u.id_rec << std::endl << std::endl;
 }
 
 template <typename T>
-void printVecStruct(std::vector<sqc<T> > v){
-    for(typename std::vector<sqc<T> >::iterator it = v.begin(); it != v.end(); it++)
+void printLstStruct(std::list<sqc<T> > v){
+    for(typename std::list<sqc<T> >::iterator it = v.begin(); it != v.end(); it++)
         printStruct(*it);
 }
 
@@ -216,52 +220,86 @@ bool    parseArgs(result<T>& r, int argc, char **argv){
 
 //INIT_VEC_SEQ
 template <typename T>
-std::vector<typename T::iterator>    initSeq(sqc<T>& u, typename T::iterator& it, typename T::iterator end, unsigned int idx_rec){
-    std::vector<typename T::iterator> vecIt;
-    // std::cout << "VEC2"<<std::endl;
+std::list<typename T::iterator>    initSeq(sqc<T>& u, typename T::iterator& it, typename T::iterator end, unsigned int idx_rec){
+    std::list<typename T::iterator> lstIt;
 
     for (unsigned int i = 0; i < idx_rec && it != end; i++)
     {
-        vecIt.push_back(it);
+        lstIt.push_back(it);
         it++;
     }
-    // std::cout << vecIt.size() <<" "<< idx_rec<<std::endl;
-    if (vecIt.size() != idx_rec) //secure not full, envoie struct
+    if (lstIt.size() != idx_rec) //secure not full, envoie struct
         u.full = false;
     else
         u.full = true;
-    return (vecIt);
+    return (lstIt);
 }
 
 template <typename T>
 sqc<T> initStructSeq(typename T::iterator& it, typename T::iterator end, int n_seq, int idx_rec){
     sqc<T> u;
-    // std::cout << "STRUCT1"<<std::endl;
  
     u.seq = initSeq<T>(u, it, end, idx_rec);
     u.id_seq = n_seq;
     u.id_rec = idx_rec;
+    if (n_seq % 2 == 0){
+        u.id_max = n_seq / 2;
+        u.id_min = 0;
+    }
+    else{
+        u.id_min = (n_seq + 1) / 2;
+        u.id_max = 0;
+    }
     return (u);
 }
 
 template <typename T>
-std::vector<sqc<T> > initVecStructSeq(result<T>& r, int idx_rec){//RETURN REF?
-    std::vector<sqc<T> > vec;
+std::list<sqc<T> > initLstStructSeq(result<T>& r, int idx_rec){//RETURN REF?
+    std::list<sqc<T> > lst;
     int n_seq = 1;
-    // std::cout << "VEC1"<<std::endl;
+
     for(typename T::iterator it = r.sorted.begin(); it != r.sorted.end(); )//IT = IT ATTENTION A LA BOUCLE INITSEQ
     {
-        vec.push_back(initStructSeq<T>(it, r.sorted.end(), n_seq, idx_rec));
+        lst.push_back(initStructSeq<T>(it, r.sorted.end(), n_seq, idx_rec));
         n_seq++;
     }
-    return (vec);
+    return (lst);
+}
+
+//UTILS
+template <typename T>
+typename std::list<T>::iterator incrIT(typename std::list<T>::iterator it, unsigned int n){
+    for (unsigned int i = 0;i < n;i++)
+        it++;
+    return (it);
 }
 
 //SORTING
 template <typename T>
-void swapRange(std::vector<typename T::iterator>& v1, std::vector<typename T::iterator>& v2){
-    typename std::vector<typename T::iterator>::iterator it1 = v1.begin();
-    typename std::vector<typename T::iterator>::iterator it2 = v2.begin();
+std::list<sqc<T> >   mergeFJ(std::list<sqc<T> > l){
+    std::list<sqc<T> > main;
+    std::list<sqc<T> > pend;
+    typename std::list<sqc<T> >::iterator it = l.begin();
+    main.push_back(*it);
+    while(++it != l.end())
+    {
+        if (it->id_max > 0 && it->full == true)//Verif full true
+            main.push_back(*it);
+        else
+            pend.push_back(*it);
+    }
+
+    std::cout << "main:";
+    printLstStruct(main);
+    std::cout << "pend:";
+    printLstStruct(pend);
+    return (main);//
+}
+
+template <typename T>
+void swapRange(std::list<typename T::iterator>& v1, std::list<typename T::iterator>& v2){
+    typename std::list<typename T::iterator>::iterator it1 = v1.begin();
+    typename std::list<typename T::iterator>::iterator it2 = v2.begin();
     while(it1 != v1.end() && it2 != v2.end())
     {
         std::iter_swap(*it1,*it2);
@@ -270,33 +308,33 @@ void swapRange(std::vector<typename T::iterator>& v1, std::vector<typename T::it
     }
 }
 
+
+
 template <typename T>
-void    sortPairs(std::vector<sqc<T> >& pairing){
-    typename std::vector<sqc<T> >::iterator it1 = pairing.begin();
-    typename std::vector<sqc<T> >::iterator it2 = it1 + 1;
+void    sortPairs(std::list<sqc<T> >& pairing){
+    typename std::list<sqc<T> >::iterator it1 = pairing.begin();
+    typename std::list<sqc<T> >::iterator it2 = incrIT<sqc<T> >(it1,1);
     while(it1 != pairing.end() && it2 != pairing.end()){
         if(*(it1->seq.back()) > *(it2->seq.back()) && it2->full == true ){//ATTENTION POTENTIELLEMENT IT2 N'EXISTE PAS
             swapRange<T>(it1->seq, it2->seq);
         }
-        it1 += 2;
+        it1 = incrIT<sqc<T> >(it1,2);
         if (it1 != pairing.end())
-            it2 += 2;
+            it2 = incrIT<sqc<T> >(it2,2);
     }
-    // printVecStruct(pairing);
-
 }
 
 template <typename T>
 bool sortFJ(result<T>& r,unsigned int idx_rec){
-    std::vector<sqc<T> > pairing = initVecStructSeq(r, idx_rec);
-    // std::cout << "idx_rec: " << idx_rec << std::endl;
-    // printVecStruct(pairing);
+    std::list<sqc<T> > pairing = initLstStructSeq(r, idx_rec);
     sortPairs<T>(pairing);
+    // printLstStruct(pairing);
     idx_rec *= 2;
-    if(idx_rec < r.sorted.size())
+    if(idx_rec < r.sorted.size() / 2)
         sortFJ(r,idx_rec);
+    mergeFJ(pairing);
     // std::cout << *(++(r.sorted.rbegin()))<< std::endl;
-    r.sorted.insert(++(r.sorted.begin()), (r.advance((r.sorted.rbegin(),3)).base(),((r.sorted.rbegin()) + 1).base());
+    // r.sorted.insert(++(r.sorted.begin()), (r.advance((r.sorted.rbegin(),3)).base(),((r.sorted.rbegin()) + 1).base());
 
     return (false);
 }
